873d592c056cd8015a489cfd8b5ffa7fe574f427
Jungbin906 <114047156+Jungbin906@users.noreply.github.com>
2026-06-28 01:34:34 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // 👇 외부 호스트(도메인) 접속 허용 설정 추가
+  server: {
+    allowedHosts: ['hello.sublumen.xyz'] 
+  },
+  preview: {
+    allowedHosts: ['hello.sublumen.xyz']
+  }
 })
